2677cef60a5cff363eb7972ebcc2a9e3d6957d2b
SemyonTelegin <44808634+SemyonTelegin@users.noreply.github.com>
2019-11-03 21:00:44 +0300

    Update stelegin_code.py

--- stelegin_code.py
+++ stelegin_code.py
@@ -1,15 +1,16 @@
 import vk_api
+from vk_api.longpoll import VkLongPoll
 import time
 import random
 import re
 import requests
 import urllib.request
 
-class Parser:
+class Parser:                           
     def __init__(self, source=""):
         self.source = str(source)
 
-    def exists(self, from_, to_):                  #Проверяет наличие обьекта между двух последовательностей символов
+    def exists(self, from_, to_):                 #Проверяет наличие чего-либо между двух последовательностей символов
         id1 = self.source.find(from_)
         if id1 == -1:
             return None
@@ -18,7 +19,7 @@ class Parser:
             return None
         return id1, id2
 
-    def unjail(self, from_, to_):           #Возвращает первое вхождение (при наличии)
+    def unjail(self, from_, to_):                       #Находит первое вхождение (если оно есть)
         if self.exists(from_, to_):
             id1, id2 = self.exists(from_, to_)
         else:
@@ -27,7 +28,7 @@ class Parser:
         self.source = self.source[:id1] + self.source[id2 + len(to_):]
         return res
 
-    def unjail_all(self, from_, to_):      #Возвращает все вхождения
+    def unjail_all(self, from_, to_):              #Находит все вхождения
         res = []
         while self.exists(from_, to_):
             res.append(self.unjail(from_, to_))
@@ -37,38 +38,41 @@ class Bot:
     def __init__(self, token=""):
         self.token = str(token)
 
-    def massage(self, resp, id):                #Загружает изображение в альбом и отправляет сообщение
+    def massage(self, resp, id, att):             #Отправляет сообщение
         upload = vk_api.VkUpload(vk)
         photo = upload.photo(r"C:\Users\iMac\PycharmProjects\Екг3\imgp.png", album_id=267125590, group_id=186913181)
         try:
-            vk.method("messages.send",
-                      {"peer_id": id, "message": resp, "random_id": random.randint(1, 2147483647), "attachmenta":'photo' + str(photo[0]['owner_id']) + '_' + str(photo[0]['id']) + ','})
+            vk.method('messages.send', {'user_id': id, 'message': response[ran], 'random_id': random.randint(1, 2147483647),"attachment":att})
         except:
             vk.method("messages.send",
                       {"peer_id": id, "message": "Не удалось распознать запрос",
                        "random_id": random.randint(1, 2147483647)})
+
 class Saver:
     def __init__(self, res):
         self.res = str(res)
 
-    def save(self):               #Сохраняет изображение по ссылке 
+    def save(self):                    #Сохраняет изображение по ссылке
         url = self.res
         img = urllib.request.urlopen(url).read()
         out = open("img.png", "wb")
         out.write(img)
-        out.close
 
 
-token = "###############################"          #Уникальный токен. Я не могу его показывать
+
+token = "##############################"          #Уникальный токен. Я не могу его показать
 base_url = 'https://yandex.ru/images/search?text='
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
 B=Bot(token)
+session = requests.Session()
 vk = vk_api.VkApi(token=token)
 vk._auth_token()
+longpoll = VkLongPoll(vk)
 while True:
     try:
         messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"})
         if messages["count"] >= 1:
+            attachments = []
             t = 0
             s = 0
             id = messages["items"][0]["last_message"]["from_id"]
@@ -92,10 +96,14 @@ while True:
             text = re.findall(base_parse, r.text)
             p = Parser(text)
             response = ['https://' + x for x in p.unjail_all('"https://', '"')]
-            ran = random.randint(13, 25)
+            ran = random.randint(20, 25)
             S=Saver(response[ran])
             S.save()
-            B.massage(response[ran], id)
+            image_url = response[ran]
+            image = session.get(image_url, stream=True)
+            photo = upload.photo_messages(photos=image.raw)[0]
+            attachments.append('photo{}_{}'.format(photo['owner_id'], photo['id']))
+            B.massage(response[ran], id, attachments)
             print(response[ran])
     except Exception as E:
         time.sleep(1)
